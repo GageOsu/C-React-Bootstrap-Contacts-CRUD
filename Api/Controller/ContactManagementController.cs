d1c413fca3ec70f93e0fd4c47d357b2aad3545ac
Gage_Osu <87873807+GageOsu@users.noreply.github.com>
2025-11-09 06:32:51 +0300

    Add ContactManagementController

---
+++ Api/Controller/ContactManagementController.cs
@@ -0,0 +1,17 @@
+using System.Data.SqlTypes;
+using Microsoft.AspNetCore.Mvc;
+
+public class ContactManagementController : BaseController
+{
+    private readonly ContactStorage storage;
+
+    public ContactManagementController(ContactStorage storage)
+    {
+        this.storage = storage;
+    }
+    [HttpPost("contacts")]
+    public void Create([FromBody] Contact contact)
+    {
+        storage.Contacts.Add(contact);
+    }
+}
