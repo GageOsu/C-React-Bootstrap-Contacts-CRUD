69fa73f6e3c63539d9f8b21139784a1b7d1c0ce1
Gage_Osu <87873807+GageOsu@users.noreply.github.com>
2025-11-09 08:51:09 +0300

    create view

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,45 @@
 import { useState } from 'react'
-import './App.css'
 
 function App() {
   return (
-    <div className='App'>
+    <div>
+      <div>
+        <div>
+          <h1>Список контактов</h1>
+        </div>
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Имя контакта</th>
+                <th>E-mail</th>
+              </tr>
+            </thead>
+            <tbody>
 
+              <tr>
+                <th>1</th>
+                <th>Имя Фамилия</th>
+                <th>example@gma.com</th>
+              </tr>
+
+              <tr>
+                <th>2</th>
+                <th>Имя Фамилия 22</th>
+                <th>example22@gma.com</th>
+              </tr>
+
+              <tr>
+                <th>3</th>
+                <th>Имя Фамилия 33</th>
+                <th>example33@gma.com</th>
+              </tr>
+
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   )
 }
