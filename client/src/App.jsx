3709ea859e2750dd2f7c604b35dfa0a1e0cf4795
Gage_Osu <87873807+GageOsu@users.noreply.github.com>
2025-11-09 09:04:39 +0300

    Выделение React-компонентов

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import TableContact from './layout/TableContact/TableContact'
 
 const App = () => {
   return (
@@ -9,36 +10,7 @@ const App = () => {
         </div>
 
         <div className="card-body">
-          <table className="table table-hover">
-            <thead>
-              <tr>
-                <th>#</th>
-                <th>Имя контакта</th>
-                <th>E-mail</th>
-              </tr>
-            </thead>
-            <tbody>
-
-              <tr>
-                <th>1</th>
-                <th>Имя Фамилия</th>
-                <th>example@gma.com</th>
-              </tr>
-
-              <tr>
-                <th>2</th>
-                <th>Имя Фамилия 22</th>
-                <th>example22@gma.com</th>
-              </tr>
-
-              <tr>
-                <th>3</th>
-                <th>Имя Фамилия 33</th>
-                <th>example33@gma.com</th>
-              </tr>
-
-            </tbody>
-          </table>
+          <TableContact />
         </div>
       </div>
     </div>
---
+++ client/src/layout/TableContact/components/RowTableContact.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const RowTableContact = () => {
+    return (
+        <tr>
+            <th>1</th>
+            <th>Имя Фамилия</th>
+            <th>example@gma.com</th>
+        </tr>
+    );
+}
+
+export default RowTableContact;
