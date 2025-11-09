6c14903f1ab3e16334990556abf2fdea6c130d89
Gage_Osu <87873807+GageOsu@users.noreply.github.com>
2025-11-09 06:50:09 +0300

    add hardcode data contact

--- Api/Storage/ContactStorage.cs
+++ Api/Storage/ContactStorage.cs
@@ -3,6 +3,16 @@ public class ContactStorage
     public ContactStorage()
     {
         this.Contacts = new List<Contact>();
+
+        for (int i = 0; i < 5; i++)
+        {
+            this.Contacts.Add(new Contact()
+            {
+                Id = i + 1,
+                Name = $"Полное имя {i + 1}",
+                Email = $"example{i + 1}@testmail.com",
+            });
+        }
     }
     public List<Contact> Contacts { get; set; }
 }
