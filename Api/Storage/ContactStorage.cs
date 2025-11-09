5f4bab203b7edcc60f0a24c3a04e522e79417406
Gage_Osu <87873807+GageOsu@users.noreply.github.com>
2025-11-09 07:13:16 +0300

    Refactoring

--- Api/Storage/ContactStorage.cs
+++ Api/Storage/ContactStorage.cs
@@ -1,5 +1,6 @@
 public class ContactStorage
 {
+    private List<Contact> Contacts { get; set; }
     public ContactStorage()
     {
         this.Contacts = new List<Contact>();
@@ -14,5 +15,50 @@ public class ContactStorage
             });
         }
     }
-    public List<Contact> Contacts { get; set; }
+
+
+    public List<Contact> GetContact()
+    {
+        return Contacts;
+    }
+
+    public void Add(Contact contact)
+    {
+        Contacts.Add(contact);
+    }
+
+    public void Remove(int id)
+    {
+        Contact contact;
+        for (int i = 0; i < Contacts.Count; i++)
+        {
+            if (this.Contacts[i].Id == id)
+            {
+                contact = Contacts[i];
+                Contacts.Remove(contact);
+                return;
+            }
+        }
+    }
+
+    public void UpdateContact(ContactDto contactDto, int id)
+    {
+        Contact contact;
+        for (int i = 0; i < Contacts.Count; i++)
+        {
+            if (Contacts[i].Id == id)
+            {
+                contact = Contacts[i];
+                if (!String.IsNullOrEmpty(contactDto.Email))
+                {
+                    contact.Email = contactDto.Email;
+                }
+                if (!String.IsNullOrEmpty(contactDto.Name))
+                {
+                    contact.Name = contactDto.Name;
+                }
+                return;
+            }
+        }
+    }
 }
