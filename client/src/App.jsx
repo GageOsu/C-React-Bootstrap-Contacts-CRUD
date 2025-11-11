5d8f441c12f89f070e23370d094d95e1d2e51ae7
Gage_Osu <87873807+GageOsu@users.noreply.github.com>
2025-11-11 23:40:05 +0300

    Установка axios и cors проблема

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,13 @@
+import axios from 'axios';
 import { useState } from 'react'
 import TableContact from './layout/TableContact/TableContact'
 import FormContact from './layout/FormContact/FormContact'
 
 const App = () => {
-
+  const url = "http://localhost:5000/api/ContactManagement/contacts";
+  axios.get(url).then(
+    res => console.log(res.data)
+  )
   const [contacts, setContacts] = useState(
     [
       { id: 1, name: "Popa Pipa", email: "example@zxc.com", },
