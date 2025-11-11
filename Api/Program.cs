f572ac4dc5fbe489cfe2708822f90522c7c2ee1c
Gage_Osu <87873807+GageOsu@users.noreply.github.com>
2025-11-12 00:11:03 +0300

    Переменные окружения и секретики

--- Api/Program.cs
+++ Api/Program.cs
@@ -10,7 +10,7 @@ opt.AddPolicy("CorsPolicy", policy =>
 {
     policy.AllowAnyMethod()
     .AllowAnyHeader()
-    .WithOrigins("http://localhost:5173");
+    .WithOrigins(args[0]);
 }));
 
 var app = builder.Build();
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,8 +3,11 @@ import { useState } from 'react'
 import TableContact from './layout/TableContact/TableContact'
 import FormContact from './layout/FormContact/FormContact'
 
+const baseApiUrl = import.meta.env.VITE_API_URL;
+console.log(baseApiUrl);
 const App = () => {
-  const url = "http://localhost:5000/api/ContactManagement/contacts";
+  const url = `${baseApiUrl}/contacts`;
+  console.log(url);
   axios.get(url).then(
     res => console.log(res.data)
   )
