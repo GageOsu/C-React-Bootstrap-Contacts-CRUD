8f526f4cfcca466274bb6473959e4f3a4ed15649
Gage_Osu <87873807+GageOsu@users.noreply.github.com>
2025-11-11 23:02:55 +0300

    Добавление формы

---
+++ client/src/layout/FormContact/FormContact.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const FormContact = (props) => {
+    return (
+        <div>
+            <div className="mb-3">
+                <form>
+                    <div className="mb-3">
+                        <label className="form-label">Введите имя:</label>
+                        <input className="form-control" type="text" />
+                    </div>
+                    <div className="mb-3">
+                        <label className="form-label">Введите почту:</label>
+                        <input className="form-control" type="text" />
+                    </div>
+                </form>
+            </div>
+
+            <div>
+                <button
+                    className="btn btn-primary"
+                    onClick={() => { props.addContact() }}
+                >
+                    Добавить контакт
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default FormContact;
