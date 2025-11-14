e37d3707992cdfbb2d4128a2bf035cca59cac11f
Gage_Osu <87873807+GageOsu@users.noreply.github.com>
2025-11-14 03:30:36 +0300

    Рефакторинг program.cs

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,18 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddControllers();
-var stringConnection = builder.Configuration.GetConnectionString("SqliteStringConnection");
-builder.Services.AddSingleton<IStorage>(new SqliteStorage(stringConnection));
+builder.Services.AddServiceCollection(builder.Configuration);
 
-builder.Services.AddCors(opt =>
-opt.AddPolicy("CorsPolicy", policy =>
-{
-    policy.AllowAnyMethod()
-    .AllowAnyHeader()
-    .WithOrigins(builder.Configuration["client"]);
-}));
+
 
 var app = builder.Build();
 app.UseSwagger();
