c6df3be52d17d972499bf1e61d5b8f31398fdd02
Gage_Osu <87873807+GageOsu@users.noreply.github.com>
2025-11-18 02:30:40 +0300

    Подготовка Frontend`a к пагинации

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import TableContact from './layout/TableContact/TableContact'
 import FormContact from './layout/FormContact/FormContact'
 import { Route, Routes, useLocation } from 'react-router-dom';
 import ContactDetails from './layout/ContactDetails/ContactDetails';
+import Pagination from './layout/Pagination/Pagination';
 
 const baseApiUrl = import.meta.env.VITE_API_URL;
 
@@ -12,14 +13,24 @@ const App = () => {
 
   const [contacts, setContacts] = useState([]);
   const location = useLocation();
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+  const [pageSize] = useState(10);
 
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  }
 
+  //page?pageNumber=1&pageSize=5
   useEffect(() => {
-    const url = `${baseApiUrl}/contacts`;
+    const url = `${baseApiUrl}/contacts/page?pageNumber=${currentPage}&pageSize=${pageSize}`;
     axios.get(url).then(
-      res => setContacts(res.data)
+      res => {
+        setContacts(res.data.contacts);
+        setTotalPages(Math.ceil(res.data.totalCount / pageSize))
+      }
     );
-  }, [location.pathname]);
+  }, [currentPage, pageSize, location.pathname]);
 
   const addContact = (contactName, contactEmail) => {
     const item = {
@@ -43,6 +54,11 @@ const App = () => {
             <div className="card-body">
               <TableContact
                 contacts={contacts}
+              />
+              <Pagination
+                currentPage={currentPage}
+                totalPages={totalPages}
+              //onPageChange={handlePageChange}
               />
               <FormContact addContact={addContact} />
             </div>
